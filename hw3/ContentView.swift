09141386480be5fef34f3302b1207c5cd072c72e
Dlin2744 <w4559731234@gmail.com>
2019-11-12 13:16:27 +0800

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by User07 on 2019/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
